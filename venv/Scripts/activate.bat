c892cc9f06700473e7d4943f8322514880298d5d
Ella Jones <ellajones2025@u.northwestern.edu>
2024-11-18 16:06:06 -0600

    Added working DreamMachine script and display qr code functionality and line dividions on player ids

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ellac\Documents\FALL 2024\Comp_sci 338\Dream-main\venv
+set VIRTUAL_ENV=C:\Users\ellac\338\DreamMachine\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
