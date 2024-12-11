8fc8b14f1c6b891c6b27b196a4f4e1202e5f11ad
Gracelyn <43764045+gracelynxs@users.noreply.github.com>
2024-12-11 00:16:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -484,5 +484,5 @@ def generate_backdrop():
         }), 500
     
 if __name__ == '__main__':
-   port = int(os.environ.get('PORT', 5000))
-   app.run(host='0.0.0.0', port=port)
+    # if running locally 
+    app.run(debug=True)
